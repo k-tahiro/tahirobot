5c41d49b802f7e8c149145a06ae1b76439452cfe
Keisuke Hirota <tahiro.k.ad@gmail.com>
2018-07-16 15:26:13 +0900

    初期ファイルを配置

---
+++ app.py
@@ -0,0 +1,50 @@
+import json
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+with open('config.json') as f:
+    config = json.load(f)
+
+line_bot_api = LineBotApi(config['access_token'])
+handler = WebhookHandler(config['secret'])
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
