aa3742517d897c4bd6da730ef701c93d9b5a0175
Keisuke Hirota <tahiro.k.ad@gmail.com>
2019-11-17 21:24:21 +0900

    Change URL and refactoring

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ handler = WebhookHandler(channel_secret)
 # TODO: deploy時に確認
 controller_host = os.environ['CONTROLLER_HOST']
 list_url = 'http://{}/codes/'.format(controller_host)
-transmit_url = 'http://{}/codes/{}'.format(controller_host, '{}')
+transmit_url = 'http://{}/codes/transmit/{}'.format(controller_host, '{}')
 
 
 @app.route("/")
@@ -61,38 +61,48 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    text = event.message.text
+    r_text = None
+
+    if '停止' in text:
+        code_key = 'stop'
+    elif '冷房' in text or '暖房' in text:
+        mode = 'c' if '冷房' in text else 'w'
+        temps = re.findall(r'[0-9]+(?:\.[0-9]+)?', text)
+        if temps:
+            code_key = '{}{}'.format(mode, temps[0])
+        else:
+            r_text = '温度を設定してね(・∀・)'
+    else:
+        r_text = '冷房なのか暖房なのかはっきりしてね(・∀・)'
+
     r = requests.get(list_url)
     codes = [
         code['code_key']
         for code in r.json()
     ]
-
-    text = event.message.text
-    if '停止' in text:
-        r = requests.post(transmit_url.format('stop'))
-        text = 'エアコン止めたわ(・∀・)' \
-            if int(r.text) == 0 else \
-            '止めるの失敗したわ(・∀・)'
-    elif '冷房' in text or '暖房' in text:
-        mode = 'c' if '冷房' in text else 'w'
-        temps = re.findall(r'[0-9]+(?:\.[0-9]+)?', text)
-        if temps:
-            key = '{}{}'.format(mode, temps[0])
-            if key in codes:
-                r = requests.post(transmit_url.format(key))
-                text = 'エアコン操作したよ(・∀・)' \
-                    if int(r.text) == 1 else \
-                    'なんか失敗したわ(・∀・)'
+    if code_key in codes:
+        r = requests.post(transmit_url.format(code_key))
+        rj = r.json()
+        if rj['success']:
+            r_text = 'エアコン操作したよ(・∀・)'
+            is_checked = rj['label'] != -1
+            is_right = (code_key == 'stop' and rj['label'] == 0) \
+                or (code_key != 'stop' and rj['label'] == 1)
+            if is_right:
+                r_text += '\n' '操作成功したわ(・∀・)'
+            elif not is_checked:
+                r_text += '\n' '結果確認できなかったわ(・∀・)'
             else:
-                text = 'データベースに設定がなかったわ(・∀・)'
+                r_text += '\n' 'うまく操作できなかったわ(・∀・)'
         else:
-            text = '温度を設定してね(・∀・)'
+            r_text = 'なんか失敗したわ(・∀・)'
     else:
-        text = '冷房なのか暖房なのかはっきりしてね(・∀・)'
+        r_text = r_text or 'データベースに設定がなかったわ(・∀・)'
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=text))
+        TextSendMessage(text=r_text))
 
 
 if __name__ == "__main__":
